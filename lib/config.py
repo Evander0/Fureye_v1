9270970a30342193305db80679a2e8e72e279358
Axw <qianyucun07@163.com>
2024-11-05 01:17:22 +0800

    enchant

--- lib/config.py
+++ lib/config.py
@@ -3,10 +3,12 @@ import json
 
 class Config:
     config = None
+    file: str = None
     default: dict = None
 
     def __init__(self, file: str, default: dict = None):
         self.default = default
+        self.file = file
         try:
             self.config = open(file, 'r+')
         except Exception as e:
@@ -31,7 +33,17 @@ class Config:
             conf.update(t)
             data = json.dumps(conf, indent=4)
             self.config.write("\n" + data)
+            self.config.flush()
         except Exception as e:
             print("读取配置文件异常")
             print("错误代码：" + str(e))
             return -1
+
+    def wipe(self):
+        data = json.dumps({}, indent=4)
+        with open(self.file, 'w') as f:
+            f.write("\n" + data)
+            f.flush()
+
+    def close(self):
+        self.config.close()
